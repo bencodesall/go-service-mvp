7568f173580e0ad9fc6347083229be2181af971e
Ben Nowak <bencodesall@gmail.com>
2021-06-25 22:11:11 -0400

    (middleware): Metrics
    - Implemented metrics middleware
    
    [learngo-1]
    
    Signed-off-by: Ben Nowak <bencodesall@gmail.com>

--- app/app-api/handlers/check.go
+++ app/app-api/handlers/check.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/bencodesall/ardanlabs-service-2.0/foundation/web"
+	"github.com/pkg/errors"
 )
 
 type check struct {
@@ -17,10 +18,10 @@ func (c check) readiness(ctx context.Context, w http.ResponseWriter, r *http.Req
 
 	if n := rand.Intn(100); n%100 == 0 {
 		// TESTS FOR MIDDLEWARE LOGGING
-		//return errors.New("untrusted error")
-		//return web.NewRequestError(errors.New("trusted error"), http.StatusBadRequest)
-		//panic("forcing panic")
-		return web.NewShutdownError("forcing shutdown")
+		// return errors.New("untrusted error")
+		return web.NewRequestError(errors.New("trusted error"), http.StatusBadRequest)
+		// panic("forcing panic")
+		// return web.NewShutdownError("forcing shutdown")
 	}
 	status := struct {
 		Status string
--- app/app-api/handlers/handlers.go
+++ app/app-api/handlers/handlers.go
@@ -1,18 +1,18 @@
 package handlers
 
 import (
-	"github.com/bencodesall/ardanlabs-service-2.0/business/middleware"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/bencodesall/ardanlabs-service-2.0/business/middleware"
 	"github.com/bencodesall/ardanlabs-service-2.0/foundation/web"
 )
 
 // API constructs an http.Handler with all application routes defined
 func API(build string, shutdown chan os.Signal, log *log.Logger) *web.App {
 
-	app := web.NewApp(shutdown, middleware.Logger(log), middleware.Errors(log), middleware.Panics(log))
+	app := web.NewApp(shutdown, middleware.Logger(log), middleware.Errors(log), middleware.Metrics(), middleware.Panics(log))
 
 	check := check{
 		log: log,
