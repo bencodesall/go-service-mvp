92ebbc0d9353f87de9c1e4704ec79e82eb3ecc2c
Ben Nowak <bencodesall@gmail.com>
2021-06-25 21:51:11 -0400

    (middleware): Panic handler implementation
    - Added `middleware.Panics()` as argument to `web.NewApp()` constructor.
    - Created middleware `panics.go`
    
    [learngo-1]
    
    Signed-off-by: Ben Nowak <bencodesall@gmail.com>

--- app/app-api/handlers/check.go
+++ app/app-api/handlers/check.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 
 	"github.com/bencodesall/ardanlabs-service-2.0/foundation/web"
-	"github.com/pkg/errors"
 )
 
 type check struct {
@@ -15,15 +14,13 @@ type check struct {
 }
 
 func (c check) readiness(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-	// NOTICE THE DECOUPLING OF VALIDATION AND ERROR HANDLING
-	//var u User
-	//if err := web.Decode(r, &u); err != nil {
-	//	return err
-	//}
 
 	if n := rand.Intn(100); n%100 == 0 {
+		// TESTS FOR MIDDLEWARE LOGGING
 		//return errors.New("untrusted error")
-		return web.NewRequestError(errors.New("trusted error"), http.StatusBadRequest)
+		//return web.NewRequestError(errors.New("trusted error"), http.StatusBadRequest)
+		//panic("forcing panic")
+		return web.NewShutdownError("forcing shutdown")
 	}
 	status := struct {
 		Status string
